11c9050dbd7a35c98f276b5cece77e0e927ce924
Neeraj Kumar <kumarn74858@gmail.com>
2025-06-07 09:52:09 +0530

    HomePage setup & ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,30 @@
-import { StrictMode } from 'react'
+import React from 'react'
+import ReactDom from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+import HomePage from './landing_page/home/HomePage'
+import PricingPage from './landing_page/pricing/PricingPage'
+import Signup from './landing_page/signup/Signup'
+import AboutPage from './landing_page/about/AboutPage'
+import SupportPage from './landing_page/support/SupportPage'
+import ProductPage from './landing_page/product/ProductPage'
+import Navbar from './landing_page/Navbar'
+import Footer from './landing_page/Footer'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <div>
-      <h1>hello</h1>
-    </div>
-  </StrictMode>,
-)
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/pricing" element={<PricingPage />} />
+        <Route path="/support" element={<SupportPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+);
+
